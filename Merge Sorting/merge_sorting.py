032f1f1aecfd2e70f4e8d9ae99c242c3051e2ad1
chiiglaaa <99479509+chiiglaaa@users.noreply.github.com>
2023-01-28 18:47:15 +0400

    Update merge_sorting.py

--- Merge Sorting/merge_sorting.py
+++ Merge Sorting/merge_sorting.py
@@ -4,10 +4,7 @@ def merge_sort(arr):
         return arr
     mid = len(arr) // 2
     left = merge_sort(arr[:mid])
-    print(left, " Left")
     right = merge_sort(arr[mid:])
-    print(right," Right")
-    print(list(sorted(left + right)), " Returner")
     return list(sorted(left + right))
 
 example = [54,26,93,17,77]
