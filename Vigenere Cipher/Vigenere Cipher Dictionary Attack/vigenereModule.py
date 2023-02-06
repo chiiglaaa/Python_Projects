919c3d358362455c8516cd9955a3d323cb5b500d
chiiglaaa <99479509+chiiglaaa@users.noreply.github.com>
2023-02-07 03:44:15 +0400

    Add files via upload

---
+++ Vigenere Cipher/Vigenere Cipher Dictionary Attack/vigenereModule.py
@@ -0,0 +1,72 @@
+def key_generator(text, alphabet, key):
+    result = ''
+    j = 0
+    if len(key) > len(text):
+        for i in range(len(text)):
+            char = text[i]
+            if j >= len(key):
+                j = 0
+            if char.lower() in alphabet:
+                char = key.lower()[j]
+                j += 1
+                result += char.lower()
+            if char not in alphabet:
+                result += char
+                j += 1
+    if len(text) == len(key):
+        return key
+    if len(text) > len(key):
+        for i in range(len(text)):
+            char = text[i]
+            if j >= len(key):
+                j = 0
+            if char.lower() in alphabet:
+                char = key.lower()[j]
+                j += 1
+                result += char.lower()
+            if char not in alphabet:
+                result += char
+                j += 1
+    return result
+
+
+def vigenere_encrypt(text, alphabet, key):
+    result = ''
+    for i in range(len(text)):
+        char = text[i]
+        cipher = key[i]
+        if char.lower() not in alphabet:
+            result += char
+            i += 1
+        if char.lower() in alphabet:
+            cipher_index = alphabet.index(cipher)
+            char_index = alphabet.index(char.lower())
+            if cipher_index == 0:
+                result += char
+            else:
+                if cipher_index + char_index >= len(alphabet):
+                    result += alphabet[(cipher_index + char_index) - len(alphabet)]
+                else:
+                    result += alphabet[cipher_index + char_index]
+    return result
+
+
+def vigenere_decrypt(text, alphabet, key):
+    result = ''
+    for i in range(len(text)):
+        char = text[i]
+        cipher = key[i]
+        if char.lower() not in alphabet:
+            result += char
+            i += 1
+        if char.lower() in alphabet:
+            cipher_index = alphabet.index(cipher.lower())
+            char_index = alphabet.index(char)
+            if cipher_index == 0:
+                result += char
+            else:
+                if cipher_index - char_index < 0:
+                    result += alphabet[-(cipher_index - char_index)]
+                else:
+                    result += alphabet[char_index - cipher_index]
+    return result
